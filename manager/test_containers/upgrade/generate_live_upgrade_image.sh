f5a3ac9c0c7db3e1c6d36b3cb2e1164cae617c39
Tomas Hehejik <thehejik@suse.com>
2022-01-12 16:10:22 +0000

    Add s390x support

--- manager/test_containers/upgrade/generate_live_upgrade_image.sh
+++ manager/test_containers/upgrade/generate_live_upgrade_image.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # make sure IMAGE wasn't used by any releases
-IMAGE="longhornio/longhorn-engine:live-upgrade-5-3-1"
+# It is retagged official longhorn-engine image with newer longhorn bin version then the installed one
+# Raul was doing a new commit in longhorn-engine repo and a new git tag rc6
+# So in the end "rancherlabs/longhornonz-longhorn-engine:v1.2.3-rc6-s390x" was retagged as $IMAGE value
+# Deployed version under test was "rancherlabs/longhornonz-longhorn-engine:v1.2.3-rc3-s390x"
+IMAGE="thehejik/longhorn-engine:live-upgrade-5-3-1"
 
 version=`docker run $IMAGE longhorn version --client-only`
 echo Image version output: $version
@@ -13,7 +17,7 @@ ControllerAPIMinVersion=`echo $version|jq -r ".clientVersion.controllerAPIMinVer
 DataFormatVersion=`echo $version|jq -r ".clientVersion.dataFormatVersion"`
 DataFormatMinVersion=`echo $version|jq -r ".clientVersion.dataFormatMinVersion"`
 
-test_image="longhornio/longhorn-test:upgrade-test.${CLIAPIVersion}-${CLIAPIMinVersion}"\
+test_image="thehejik/longhorn-test:upgrade-test.${CLIAPIVersion}-${CLIAPIMinVersion}"\
 ".${ControllerAPIVersion}-${ControllerAPIMinVersion}"\
 ".${DataFormatVersion}-${DataFormatMinVersion}"
 
